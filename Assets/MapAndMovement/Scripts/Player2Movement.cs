64992cc24e195052d91ac284f77b3e49c06eb8a2
JoshTyan <tyanj@uci.edu>
2023-05-07 15:33:34 -0700

    boy idle animations
    boy idle animations for facing left and right based on key input, updated player movement script (girl) for animations

--- Assets/MapAndMovement/Scripts/Player2Movement.cs
+++ Assets/MapAndMovement/Scripts/Player2Movement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//BOY
 public class Player2Movement : MonoBehaviour
 {
     Rigidbody2D rb;
@@ -10,6 +12,13 @@ public class Player2Movement : MonoBehaviour
 
     [SerializeField]
     private LayerMask jumpableGround;
+
+    //animation variables
+    Vector2 movement;
+    public Animator animator;
+    float lastMoveX;
+    float lastMoveY;
+    float curSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -25,23 +34,39 @@ public class Player2Movement : MonoBehaviour
         float dirX = Input.GetAxis("P2_Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
-        /*
-        if(this.gameObject.tag == "Player 2")
-        {
-            float dirX = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-        }
-        */
-
         //if(Input.GetButtonDown("Jump") && isGrounded())
         if(Input.GetKeyDown("up") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, 7f);
         }
+
+        //for animation controls
+        movement.x = Input.GetAxisRaw("P2_Horizontal");
+        movement.y = Input.GetAxisRaw("P2_Vertical");
+
+        animationControls();
     }
 
     private bool isGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    void animationControls()
+    {
+        curSpeed = movement.sqrMagnitude;
+        if(curSpeed > 0.01)
+        {
+            lastMoveX = movement.x;
+            lastMoveY = movement.y;
+        }
+        
+        
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Horizontal", lastMoveX);
+        animator.SetFloat("Vertical", lastMoveY);
+        
+        animator.SetFloat("Speed", curSpeed);
     }
 }
--- Assets/MapAndMovement/Scripts/PlayerMovement.cs
+++ Assets/MapAndMovement/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//GIRL
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
@@ -10,6 +12,13 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private LayerMask jumpableGround;
+
+    //animation variables
+    Vector2 movement;
+    public Animator animator;
+    float lastMoveX;
+    float lastMoveY;
+    float curSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -24,28 +33,40 @@ public class PlayerMovement : MonoBehaviour
         
         float dirX = Input.GetAxis("P1_Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-        
-        /*
-        if(this.gameObject.tag == "Player 1")
-        {
-            if (Input.GetKey("a") || Input.GetKey("d"))
-            {
-                //float dirX = Input.GetAxis("Horizontal");
-                //rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-                rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
-            }
-        }
-        */
 
         //if(Input.GetButtonDown("Jump") && isGrounded())
         if(Input.GetKeyDown("w") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, 7f);
         }
+
+        //for animation controls
+        movement.x = Input.GetAxisRaw("P1_Horizontal");
+        movement.y = Input.GetAxisRaw("P1_Vertical");
+
+        animationControls();
     }
 
     private bool isGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    void animationControls()
+    {
+        curSpeed = movement.sqrMagnitude;
+        if(curSpeed > 0.01)
+        {
+            lastMoveX = movement.x;
+            lastMoveY = movement.y;
+        }
+        
+        
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Horizontal", lastMoveX);
+        animator.SetFloat("Vertical", lastMoveY);
+        
+        animator.SetFloat("Speed", curSpeed);
     }
 }
